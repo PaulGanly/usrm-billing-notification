a12b7761de2ab5d14e74aa2818e9e6cbe48f3c11
Paul <PaulGanly@users.noreply.github.com>
2023-09-27 14:30:31 -0400

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,10 @@
 class Utils {
 
   public isValidEmail(str: string): boolean {
-    return str && /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str);
+    return str && str.includes('@libertymutual.com');
   }
 
   public isValidUSZipCode(str: string): boolean {
-    return str && /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(str);
+    return str && str.length == 5 && !isNaN(str);
   }
 }
